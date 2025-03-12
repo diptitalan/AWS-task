2491d3ae552a555ca23f7b2e2ceae371da293fdc
Dipti-Talan <dipti_talan@epam.com>
2025-03-12 19:39:35 +0530

    deleted

--- task04/app/lambdas/sns_handler/index.js
+++
@@ -1,7 +0,0 @@
-exports.handler = async (event) => {
-    event.Records.forEach(record => {
-        const snsMessage = record.Sns.Message;
-        console.log(`Received SNS message: ${snsMessage}`);
-    });
-    return { statusCode: 200 };
-};
--- task04/app/lambdas/sqs_handler/index.js
+++
@@ -1,6 +0,0 @@
-exports.handler = async (event) => {
-    event.Records.forEach(record => {
-        console.log(`Received SQS message: ${record.body}`);
-    });
-    return { statusCode: 200 };
-};
